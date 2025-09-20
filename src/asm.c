51ef01e111310bd0c303966ba8083d512d4d5589
malloc-nbytes <zdhdev@yahoo.com>
2025-09-20 00:05:54 -0500

    add simple register allocator

--- src/asm.c
+++ src/asm.c
@@ -9,9 +9,96 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+calling order: rdi, rsi, rdx, rcx, r8, r9.
+
+8-byte register Bytes 0-3 Bytes 0-1 Byte 0
+  %rax %eax %ax %al
+  %rcx %ecx %cx %cl
+  %rdx %edx %dx %dl
+  %rbx %ebx %bx %bl
+  %rsi %esi %si %sil
+  %rdi %edi %di %dil
+  %rsp %esp %sp %spl
+  %rbp %ebp %bp %bpl
+  %r8 %r8d %r8w %r8b
+  %r9 %r9d %r9w %r9b
+  %r10 %r10d %r10w %r10b
+  %r11 %r11d %r11w %r11b
+  %r12 %r12d %r12w %r12b
+  %r13 %r13d %r13w %r13b
+  %r14 %r14d %r14w %r14b
+  %r15 %r15d %r15w %r15b
+ */
+
+#define REGAT(i, j, mat) mat[(i) * g_regs_c + (j)]
+
+static const char *g_regs[] = {
+        "rax", "eax",  "ax",   "al",
+        "rcx", "ecx",  "cx",   "cl",
+        "rdx", "edx",  "dx",   "dl",
+        "rsi", "esi",  "si",   "sil",
+        "rdi", "edi",  "di",   "dil",
+        "r8",  "r8d",  "r8w",  "r8b",
+        "r9",  "r9d",  "r9w",  "r9b",
+        "r10", "r10d", "r10w", "r10b",
+        "r11", "r11d", "r11w", "r11b"
+};
+
+const size_t g_regs_n = sizeof(g_regs)/sizeof(*g_regs);
+#define g_regs_r 9
+#define g_regs_c 4
+
+static int g_inuse_regs[g_regs_r * g_regs_c] = {
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+};
+
 typedef struct {
         FILE *out;
 } asm_context;
+
+static int
+alloc_reg(int sz)
+{
+        for (size_t i = 0; i < g_regs_r; ++i) {
+                int ok = 1;
+                for (size_t j = 0; j < g_regs_c; ++j) {
+                        if (REGAT(i, j, g_inuse_regs)) {
+                                ok = 0;
+                                break;
+                        }
+                }
+                if (!ok) continue;
+                switch (sz) {
+                case 8:
+                        REGAT(i, 0, g_inuse_regs) = 1;
+                        return i * g_regs_c + 0;
+                        //return REGAT(i, 0, g_regs);
+                case 4:
+                        REGAT(i, 1, g_inuse_regs) = 1;
+                        return i * g_regs_c + 1;
+                        //return REGAT(i, 1, g_regs);
+                case 2:
+                        REGAT(i, 2, g_inuse_regs) = 1;
+                        return i * g_regs_c + 2;
+                        //return REGAT(i, 2, g_regs);
+                case 1:
+                        REGAT(i, 3, g_inuse_regs) = 1;
+                        return i * g_regs_c + 3;
+                        //return REGAT(i, 3, g_regs);
+                default: forge_err_wargs("alloc_reg(): cannot alloc register with size %d", sz);
+                }
+        }
+        forge_err("alloc_reg(): no more registers");
+}
 
 static void
 write_txt(asm_context *ctx,
